79caff131dcd36676150e944353c1413e3f012dd
patel7vishal <pv251023@gmail.com>
2024-01-02 23:44:35 +0530

    init

---
+++ dist/mintinConfig.d.ts
@@ -0,0 +1,3 @@
+export declare const mintingConfig: {
+    collectionId: string;
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { web3 } from "@project-serum/anchor";
+import { Metaplex } from "@metaplex-foundation/js";
+
+const log = console.log;
+log("hi")
